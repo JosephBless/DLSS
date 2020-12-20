ce7674566d257ad44065e8aa20980fa6f3c2b4c3
Vee Upatising <veeupatising@gmail.com>
2020-12-19 19:05:42 -0500

    Python 3 Update

--- Load Model and Analyze Results.py
+++ Load Model and Analyze Results.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[6]:
+# In[8]:
 
 
 import os
@@ -14,14 +14,34 @@ from keras.models import load_model
 
 # # Parameters
 
-# In[7]:
+# In[21]:
 
 
-model_path = r'C:\Downloads\model.h5'
-dataset_path = r'C:\Downloads\Dataset'
+# Dimensions of the images inside the dataset.
+input_dimensions = (128,128,3)
+
+# Dimensions of the images inside the dataset.
+output_dimensions = (256,256,3)
+
+# The ratio of the difference in size of the two images. Used for setting ratio of image subplots
+super_sampling_ratio = int(output_dimensions[0] / input_dimensions[0])
+
+# Path to saved .h5 model
+model_path = r'C:\Users\Vee\Desktop\generator90.h5'
+
+# Path to folder containing images to super sample
+dataset_path = r'C:\Users\Vee\Desktop\pokemon'
+
+# Folder where you want to save to model as well as generated samples
+save_path = r'C:\Users\Vee\Desktop'
+
+# Boolean flag, set to True if the data has pngs to remove alpha layer from images
+png = True
 
 
-# In[8]:
+# # Load Model
+
+# In[19]:
 
 
 model = load_model(model_path)
@@ -29,40 +49,59 @@ model = load_model(model_path)
 
 # # Load Images and Super Sample
 
-# In[10]:
+# In[29]:
 
 
 paths = []
+count = 0
+
 for r, d, f in os.walk(dataset_path):
     for file in f:
         if '.png' in file or 'jpg' in file:
             paths.append(os.path.join(r, file))
-count = 0
+
 for path in paths:
-    #select image
+    
+    # Select image
     img = Image.open(path)
 
     #create plot
-    f, axarr = plt.subplots(1,3,figsize=(15,15),gridspec_kw={'width_ratios': [1,4,4]})
-    axarr[0].set_xlabel('Original Image (64x64)', fontsize=10)
-    axarr[1].set_xlabel('Interpolated Image (256x256)', fontsize=10)
-    axarr[2].set_xlabel('Super Sampled Image (256x256)', fontsize=10)
+    f, axarr = plt.subplots(1,3,figsize=(15,15),gridspec_kw={'width_ratios': [1,super_sampling_ratio,super_sampling_ratio]})
+    axarr[0].set_xlabel('Original Image (' + str(input_dimensions[0]) + 'x' + str(input_dimensions[1]) + ')', fontsize=10)
+    axarr[1].set_xlabel('Interpolated Image (' + str(output_dimensions[0]) + 'x' + str(output_dimensions[1]) + ')', fontsize=10)
+    axarr[2].set_xlabel('Super Sampled Image (' + str(output_dimensions[0]) + 'x' + str(output_dimensions[1]) + ')', fontsize=10)
 
     #original image
-    x = img.resize((64,64))
+    x = img.resize((input_dimensions[0],input_dimensions[1]))
+    
     #interpolated (resized) image
-    y = x.resize((256,256))
-    #plotting first two images
+    y = x.resize((output_dimensions[0],output_dimensions[1]))
+    
+    
     x = np.array(x)
     y = np.array(y)
+    
+    # Remove alpha layer if imgaes are PNG
+    if(png):
+        x = x[...,:3]
+        y = y[...,:3]
+    
+    #plotting first two images
     axarr[0].imshow(x)
     axarr[1].imshow(y)
+    
     #plotting super sampled image
-    x = x.reshape(1,64,64,3)/256
-    result = np.array(model.predict_on_batch(x))*256
-    result = result.reshape(256,256,3)
-    result = result.astype(int)
+    x = x.reshape(1,input_dimensions[0],input_dimensions[1],input_dimensions[2])/255
+    result = np.array(model.predict_on_batch(x))*255
+    result = result.reshape(output_dimensions[0],output_dimensions[1],output_dimensions[2])
+    np.clip(result, 0, 255, out=result)
+    result = result.astype('uint8')
+                
     axarr[2].imshow(result)
-    f.savefig('frame_%d.png' % count)
+    
+    # Save image
+    f.savefig(save_path + '\\frame_%d.png' % count)
+    
+    # Increment file name counter
     count = count + 1
 
